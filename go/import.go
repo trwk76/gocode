e74070a006a096dca92e8bdccb6677fdd0ec8259
Thomas KIPP <thomas.kipp@outlook.com>
2024-06-20 12:02:51 +0000

    Imports.

--- go/import.go
+++ go/import.go
@@ -3,6 +3,8 @@ package golang
 import (
 	"fmt"
 	"go/token"
+	"os"
+	"path/filepath"
 	"slices"
 	"sort"
 	"strconv"
@@ -15,37 +17,23 @@ type (
 	Imports []Import
 
 	Import struct {
-		comm      string
-		alias     string
-		path      string
-		aliasExpl bool
+		comm  string
+		alias string
+		path  string
 	}
 )
 
 func (i *Imports) Ensure(pkgPath string, alias string, comment string) Import {
-	expl := false
-
-	if alias != "" {
-		expl = true
-	} else {
-		alias, expl = detectPkgAlias(pkgPath)
-	}
-
 	if !isImportAlias(alias) {
 		panic(fmt.Errorf("'%s' is not a valid package alias", alias))
 	}
 
-	for _, itm := range *i {
-		if itm.alias == alias {
-			panic(fmt.Errorf("import alias '%s' is already used by package '%s'", alias, itm.path))
-		}
-	}
+	alias = i.uniqueAlias(alias)
 
 	res := Import{
 		comm:      comment,
 		alias:     alias,
 		path:      pkgPath,
-		aliasExpl: expl,
 	}
 
 	*i = append(*i, res)
@@ -62,12 +50,8 @@ func (i Imports) write(w *code.Writer) {
 		}
 
 		w.WriteString("import ")
-
-		if i[0].alias != "" {
-			w.WriteString(i[0].alias)
-			w.Space()
-		}
-
+		w.WriteString(i[0].alias)
+		w.Space()
 		w.WriteString(strconv.Quote(i[0].path))
 		w.Newline()
 		return
@@ -86,7 +70,6 @@ func (i Imports) write(w *code.Writer) {
 
 	prvSys := false
 	rows := make([]code.Row, 0, len(i))
-	hasAlias := i.hasAlias()
 
 	for _, itm := range i {
 		if prvSys && !isSysPkg(itm.path) {
@@ -94,7 +77,7 @@ func (i Imports) write(w *code.Writer) {
 			rows = append(rows, code.Row{})
 		}
 
-		rows = append(rows, itm.row(hasAlias))
+		rows = append(rows, itm.row())
 	}
 
 	w.WriteString("import (")
@@ -108,26 +91,11 @@ func (i Imports) write(w *code.Writer) {
 	w.Newline()
 }
 
-func (i Imports) hasAlias() bool {
-	return slices.ContainsFunc(i, func(i Import) bool { return i.aliasExpl })
-}
-
-func (i Import) row(hasAlias bool) code.Row {
+func (i Import) row() code.Row {
 	var res code.Row
 
 	res.Prefix = commentRenderer(i.comm)
-
-	if hasAlias {
-		alias := ""
-
-		if i.aliasExpl {
-			alias = i.alias
-		}
-
-		res.Cols = []string{alias, strconv.Quote(i.path)}
-	} else {
-		res.Cols = []string{strconv.Quote(i.path)}
-	}
+	res.Cols = []string{i.alias, strconv.Quote(i.path)}
 
 	return res
 }
@@ -136,19 +104,10 @@ func isSysPkg(path string) bool {
 	return !strings.ContainsAny(path, ".")
 }
 
-func detectPkgAlias(path string) (string, bool) {
-	res := path
-
-	if idx := strings.LastIndexByte(path, '/'); idx >= 0 {
-		res = path[idx+1:]
-	}
-
-	if isImportAlias(res) {
-		return res, false
-	}
-
-}
-
 func isImportAlias(alias string) bool {
+	if alias == "_" {
+		return true
+	}
+
 	return token.IsIdentifier(alias) && !token.IsExported(alias)
 }
